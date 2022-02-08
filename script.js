e4dbe5dcfdbf8449dcf4486918c20adfa0372320
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-08 11:51:59 +0600

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function makeRed(){
     document.body.style.backgroundColor='red';
 }
+function makeBlue(){
+    document.body.style.backgroundColor='blue';
+}
+
+function makeBlack(){
+    document.body.style.backgroundColor='black'; 
+}
+
