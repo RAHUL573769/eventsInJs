ef9ea84f3a222c71aea80eef3256bb22e44e7579
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-08 12:16:59 +0600

    first commit

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ function makeBlack(){
     document.body.style.backgroundColor='black'; 
 }
 
+
+// evebt listener adding
+
+const green=document.getElementById('a');
+console.log(green);
+green.onclick = function(){
+    document.body.style.backgroundColor='green'; 
+}
